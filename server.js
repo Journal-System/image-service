fcdffbded8b122ce063dbfc4204bca50376c6f96
Tomteluvan <mickebros@gmail.com>
2023-11-28 23:27:55 +0100

    connecting to db and initial dependecies

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
-const mysql = require('mysql')
-const express = require('express')
+const mysql = require('mysql2')
+const express = require('express') //multer + cors
 require('dotenv').config();
-
-console.log(process.env.MYSQL_PASSWORD)
 
 const pool = mysql.createPool({
     host: '127.0.0.1',
     user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD
-
+    password: process.env.MYSQL_PASSWORD,
+    database: 'PatientInfo'
 })
+
+pool.query("SELECT * FROM user", (error, results, fields) => {
+    if (error) {
+        console.error('Error executing query:', error.message);
+        return;
+    }
+
+    console.log('Query results:', results);
+});
