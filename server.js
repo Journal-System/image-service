ad730ae7b63f286b206cecdf8b71c88473bc6f81
Tomteluvan <mickebros@gmail.com>
2023-12-01 15:00:25 +0100

    upload/download images
    Can now upload/download images
    Problem is that png is hardcoded for downloading, we could try to limit the upload to only allow png in the frontend

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 const mysql = require('mysql2')
-const express = require('express') //multer + cors
+const multer = require('multer')
+const express = require('express')
+const cors = require('cors')
 require('dotenv').config();
+
+
+const app = express()
+const port = 8084
+
+const storage = multer.memoryStorage();
+const upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: 1024*1024*5, //max 5mb file, 1kb * 1kb = 1mb and then * 5 for 5mb base is bytes used in multer
+    },
+})
 
 const pool = mysql.createPool({
     host: '127.0.0.1',
@@ -9,11 +23,65 @@ const pool = mysql.createPool({
     database: 'PatientInfo'
 })
 
-pool.query("SELECT * FROM user", (error, results, fields) => {
+pool.query(`
+    CREATE TABLE IF NOT EXISTS images (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        data LONGBLOB
+    )
+`, (error, results, fields) => {
     if (error) {
-        console.error('Error executing query:', error.message);
+        console.error('Error creating "images" table:', error.message);
         return;
     }
+    console.log('Table "images" created or already exists');
+});
 
-    console.log('Query results:', results);
+app.use(express.json())
+
+app.use(cors({
+    origin: 'http://localhost:4000', // Allow requests only from localhost:4000
+    credentials: true, // Enable credentials (cookies, authorization headers, etc.)
+}));
+
+app.post('/upload', upload.single('image'), (req, res) => {
+    const imageData = req.file.buffer; // The image data in a Buffer
+
+    // Save the image data to the database
+    pool.query('INSERT INTO images (data) VALUES (?)', [imageData], (error, results, fields) => {
+        if (error) {
+            console.error('Error saving image:', error.message);
+            res.status(500).send('Internal Server Error');
+            return;
+        }
+
+        res.status(200).send('Image uploaded successfully');
+    });
+});
+
+app.get('/download/:imageId', (req, res) => {
+    const imageId = req.params.imageId;
+
+    // Retrieve image data from the database based on the imageId
+    pool.query('SELECT data FROM images WHERE id = ?', [imageId], (error, results, fields) => {
+        if (error) {
+            console.error('Error retrieving image:', error.message);
+            res.status(500).send('Internal Server Error');
+            return;
+        }
+
+        if (results.length === 0) {
+            res.status(404).send('Image not found');
+            return;
+        }
+
+        const imageData = results[0].data;
+
+        // Set the appropriate headers for image response
+        res.setHeader('Content-Type', 'image/png'); // Change content type based on your image type
+        res.send(imageData);
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
 });
