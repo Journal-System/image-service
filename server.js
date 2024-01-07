8a2d56fc562bb1998e0ead388c05f706dca7a0e5
Tomteluvan <mickebros@gmail.com>
2024-01-07 19:22:30 +0100

    allowing all origin cors

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ pool.query(`
 app.use(express.json())
 
 const corsOptions = {
-    origin: 'https://patient-journal.app.cloud.cbh.kth.se',
+    origin: '*',
     credentials: true,
     methods: 'GET,POST,PUT,OPTIONS',
     allowedHeaders: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
