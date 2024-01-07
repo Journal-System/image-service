a1a4c3f859c7379ef1794a130e79456c25b8960d
Tomteluvan <mickebros@gmail.com>
2024-01-07 19:46:10 +0100

    order of app use

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ const keycloak = require('./config/keycloak-config.js').initKeycloak();
 const app = express()
 const port = 8084
 
+app.use(keycloak.middleware());
+
 app.use(session({
     secret: 'bDphhdOrw0VDIU0O1zoxPU6dM9ZvFCMM', // Replace with your secret key
     resave: false,
@@ -18,7 +20,6 @@ app.use(session({
     store: new session.MemoryStore() // Or another session store for production
 }));
 
-app.use(keycloak.middleware());
 
 const storage = multer.memoryStorage();
 const upload = multer({
